f1dfe09c63315b348199dd7d26904296ff241d5c
Jeff Lin <jeff.lin@minaik.com.tw>
2021-06-04 11:56:50 +0800

    Add C# BackgroundWorkerDemo

---
+++ C#/BackgroundWorkerDemo/BackgroundWorkerDemo/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace BackgroundWorkerDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 78);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(156, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(73, 9);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(33, 12);
+            this.lblMsg.TabIndex = 1;
+            this.lblMsg.Text = "label1";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 49);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(93, 49);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(180, 111);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "BackgroundWorker ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ C#/BackgroundWorkerDemo/BackgroundWorkerDemo/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace BackgroundWorkerDemo
+{
+    public partial class Form1 : Form
+    {
+        private BackgroundWorker bw;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            initProgressBar();
+            initBackgroundWorker();
+        }
+
+        private void initProgressBar()
+        {
+            progressBar1.Step = 1;
+        }
+
+        private void initBackgroundWorker()
+        {
+            bw = new BackgroundWorker();
+            bw.WorkerReportsProgress = true;
+            bw.WorkerSupportsCancellation = true;
+            bw.DoWork += new DoWorkEventHandler(bw_DoWork);
+            bw.ProgressChanged += new ProgressChangedEventHandler(bw_ProgressChanged);
+            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_RunWorkerCompleted);
+        }
+
+        //背景執行
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 1; (i <= 10); i++)
+            {
+                if ((bw.CancellationPending == true))
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    // 使用sleep模擬運算時的停頓
+                    System.Threading.Thread.Sleep(500);
+                    bw.ReportProgress((i * 10));
+                }
+            }
+        }
+
+        //處理進度
+        private void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            this.lblMsg.Text = e.ProgressPercentage.ToString();
+        }
+
+        //執行完成
+        private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if ((e.Cancelled == true))
+            {
+                this.lblMsg.Text = "取消!";
+            }
+            else if (!(e.Error == null))
+            {
+                this.lblMsg.Text = ("Error: " + e.Error.Message);
+            }
+            else
+            {
+                this.lblMsg.Text = "完成!";
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (bw.IsBusy != true)
+            {
+                this.lblMsg.Text = "開始";
+                this.progressBar1.Value = 0;
+                bw.RunWorkerAsync();
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (bw.WorkerSupportsCancellation == true)
+            {
+                bw.CancelAsync();
+            }
+        }
+    }
+}
